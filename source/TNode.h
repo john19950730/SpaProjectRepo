b0a4f42292df67fa6a1ac664fb8dfdb7948d461d
Joshua <blaqperformance@gmail.com>
2018-09-02 14:16:57 +0800

    Added parser

--- source/TNode.h
+++ source/TNode.h
@@ -1,5 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class TNode
 {
 public:
+	static TNode leftChild;
+	static TNode rightChild;
+	static TNode parent;
+	static string lineNum;
+	static string data;
+
+
 
 };
