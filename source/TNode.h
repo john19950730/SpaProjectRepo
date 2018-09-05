0ed8cb4033ce7278e1608b09f6f947a0f9280fbd
Huang Chi Ying <e0032341@u.nus.edu>
2018-09-05 22:41:15 +0800

    Remove all implementations of TNode

--- source/TNode.h
+++ source/TNode.h
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#include <string>
-
-using namespace std;
-
-class TNode
-{
-protected:
-	string type;
-	string value;
-public:
-	TNode() : type(""), value("") {}
-	TNode(string type, string value) : type(type), value(value) {}
-	TNode *parent;
-	TNode *left;
-	TNode *right;
-};
+class TNode;
