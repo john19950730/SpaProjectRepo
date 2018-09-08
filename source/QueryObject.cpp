d903981549d086006105d08e7455d9bcb0a2b1d3
Poh Kai Jun <kai_jun96@hotmail.com>
2018-09-08 15:59:49 +0800

    added queryobject

---
+++ source/QueryObject.cpp
@@ -0,0 +1,81 @@
+#include "QueryObject.h"
+
+using namespace std;
+
+QueryObject::QueryObject() {
+}
+
+QueryObject::QueryObject(STMT_RS_CLAUSE *followsClause, STMT_PROC_VAR_RS_CLAUSE *modifiesClause,
+	STMT_RS_CLAUSE *parentClause, SELECT_VAR_CLAUSE *selectClause, STMT_PROC_VAR_RS_CLAUSE *usesClause) {
+	this->setFollowsClause(followsClause);
+	this->setModifiesClause(modifiesClause);
+	this->setParentClause(parentClause);
+	this->setSelectClause(selectClause);
+	this->setUsesClause(usesClause);
+}
+
+void QueryObject::setFollowsClause(STMT_RS_CLAUSE *followsClause) {
+	this->FOLLOWS_CLAUSE = followsClause;
+}
+
+void QueryObject::setModifiesClause(STMT_PROC_VAR_RS_CLAUSE *modifiesClause) {
+	this->MODIFIES_CLAUSE = modifiesClause;
+}
+
+void QueryObject::setParentClause(STMT_RS_CLAUSE *parentClause) {
+	this->PARENT_CLAUSE = parentClause;
+}
+
+void QueryObject::setSelectClause(SELECT_VAR_CLAUSE *selectClause) {
+	this->SELECT_CLAUSE = selectClause;
+}
+
+void QueryObject::setUsesClause(STMT_PROC_VAR_RS_CLAUSE *usesClause) {
+	this->USES_CLAUSE = usesClause;
+}
+
+STMT_RS_CLAUSE* QueryObject::getFollowsClause() {
+	return FOLLOWS_CLAUSE;
+}
+
+STMT_PROC_VAR_RS_CLAUSE* QueryObject::getModifiesClause() {
+	return MODIFIES_CLAUSE;
+}
+
+STMT_RS_CLAUSE* QueryObject::getParentClause() {
+	return PARENT_CLAUSE;
+}
+
+SELECT_VAR_CLAUSE* QueryObject::getSelectClause() {
+	return SELECT_CLAUSE;
+}
+
+STMT_PROC_VAR_RS_CLAUSE* QueryObject::getUsesClause() {
+	return USES_CLAUSE;
+}
+
+bool QueryObject::hasClauses() {
+	if (MODIFIES_CLAUSE == NULL && USES_CLAUSE == NULL && FOLLOWS_CLAUSE == NULL && PARENT_CLAUSE == NULL)
+		return false;
+	return true;
+}
+
+bool QueryObject::hasFollowsClause() {
+	if (FOLLOWS_CLAUSE == NULL) return false;
+	return true;
+}
+
+bool QueryObject::hasModifiesClause() {
+	if (MODIFIES_CLAUSE == NULL) return false;
+	return true;
+}
+
+bool QueryObject::hasParentClause() {
+	if (PARENT_CLAUSE == NULL) return false;
+	return true;
+}
+
+bool QueryObject::hasUsesClause() {
+	if (USES_CLAUSE == NULL) return false;
+	return true;
+}
---
+++ source/QueryObject.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "QueryObjStructures.h"
+#include <string>
+
+using namespace std;
+
+class QueryObject {
+private:
+	SELECT_VAR_CLAUSE *SELECT_CLAUSE = NULL;
+	STMT_PROC_VAR_RS_CLAUSE *MODIFIES_CLAUSE = NULL;
+	STMT_PROC_VAR_RS_CLAUSE *USES_CLAUSE = NULL;
+	STMT_RS_CLAUSE *FOLLOWS_CLAUSE = NULL;
+	STMT_RS_CLAUSE *PARENT_CLAUSE = NULL;
+public:
+	// Constructors
+	QueryObject();
+	QueryObject(STMT_RS_CLAUSE *followsClause, STMT_PROC_VAR_RS_CLAUSE *modifiesClause, STMT_RS_CLAUSE *parentClause,
+		SELECT_VAR_CLAUSE *selectClause, STMT_PROC_VAR_RS_CLAUSE *usesClause);
+
+	// Setter methods
+	void setSelectClause(SELECT_VAR_CLAUSE *selectClause);
+	void setModifiesClause(STMT_PROC_VAR_RS_CLAUSE *modifiesClause);
+	void setUsesClause(STMT_PROC_VAR_RS_CLAUSE *usesClause);
+	void setFollowsClause(STMT_RS_CLAUSE *followsClause);
+	void setParentClause(STMT_RS_CLAUSE *parentClause);
+
+	// Getter methods
+	SELECT_VAR_CLAUSE* getSelectClause();
+	STMT_PROC_VAR_RS_CLAUSE* getModifiesClause();
+	STMT_PROC_VAR_RS_CLAUSE* getUsesClause();
+	STMT_RS_CLAUSE* getFollowsClause();
+	STMT_RS_CLAUSE* getParentClause();
+
+	// Checking methods
+	bool hasClauses();
+	bool hasModifiesClause();
+	bool hasUsesClause();
+	bool hasFollowsClause();
+	bool hasParentClause();
+};
