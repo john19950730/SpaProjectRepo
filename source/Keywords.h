e2fc107e11599b72050f5a87d999c9344f3e279e
Poh Kai Jun <kai_jun96@hotmail.com>
2018-09-08 15:58:17 +0800

    added pql keywords to keywords.h file

--- source/Keywords.h
+++ source/Keywords.h
@@ -6,4 +6,12 @@
 namespace keywords {
 	const std::string PROCEDURE = "procedure";
 	const std::string ASSIGNMENT = "=";
+
+	namespace query {
+		const std::string ASSIGNMENT_VAR = "assign";
+		const std::string VARIABLE_VAR = "variable";
+		const std::string STMT_VAR = "stmt";
+		const std::string PROC_VAR = "procedure";
+		const std::string WHILE_VAR = "while";
+	}
 };
