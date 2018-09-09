2961068c20b3ed6dd196f220ea29579ac482ecbe
Poh Kai Jun <kai_jun96@hotmail.com>
2018-09-10 00:42:42 +0800

    add methods to query evaluator

--- source/QueryEvaluator.cpp
+++ source/QueryEvaluator.cpp
@@ -6,18 +6,61 @@
 using namespace std;
 using namespace keywords::query;
 
-string QueryEvaluator::evaluateQueryObject(QueryObject *queryObject) {
+QueryEvaluator::QueryEvaluator(QueryObject *queryObject) {
+	this->queryObject = queryObject;
+}
+
+string QueryEvaluator::evaluateQueryObject() {
+	if (!queryObject->hasClauses()) return selectImmediateResults();
+
+	// First milestone - evaluate only one clause
+	if (queryObject->getUsesClause() != NULL) return evaluateUsesClause();
+	else if (queryObject->getModifiesClause() != NULL) return evaluateModifiesClause();
+	else if (queryObject->getParentClause() != NULL) return evaluateParentClause();
+	else if (queryObject->getFollowsClause() != NULL) return evaluateFollowsClause();
+	
+	return "Error";
+}
+
+// Return results immediately if there are no clauses
+string QueryEvaluator::selectImmediateResults() {
 	SELECT_VAR_CLAUSE *selectClause = queryObject->getSelectClause();
 	string varName = selectClause->variableName;
 	string varType = selectClause->variableType;
 
-	// Return results immediately if there are no clauses
-	if (!queryObject->hasClauses()) {
-		if (varType == ASSIGNMENT_VAR) return "allAssignmentStmts";
-		else if (varType == VARIABLE_VAR) return "allVariableStmts";
-		else if (varType == STMT_VAR) return "allStmts";
-		else if (varType == PROC_VAR) return "allProcedures";
+	if (varType == ASSIGNMENT_VAR) return "allAssignmentStmts";
+	else if (varType == VARIABLE_VAR) return "allVariableStmts";
+	else if (varType == STMT_VAR) return "allStmts";
+	else if (varType == PROC_VAR) return "allProcedures";
+
+	return "";
+}
+
+string QueryEvaluator::evaluateUsesClause() {
+	STMT_PROC_VAR_RS_CLAUSE *usesClause = queryObject->getUsesClause();
+	string firstEntity = usesClause->firstEntity; // check if the first parameter is a stmtNo or synonym
+
+	if (isInteger(firstEntity)) {
+		int parsedFirstEntity = stoi(firstEntity);
+		string api = "isUses(parsedFirstEntity, varname)"; // check if the clause is true
+		return selectImmediateResults(); // return the "select" immediately if its true
 	}
 
 	return "";
+}
+
+string QueryEvaluator::evaluateModifiesClause() {
+	return "";
+}
+
+string QueryEvaluator::evaluateParentClause() {
+	return "";
+}
+
+string QueryEvaluator::evaluateFollowsClause() {
+	return "";
+}
+
+bool QueryEvaluator::isInteger(string str) {
+	return str.find_first_not_of("0123456789") == string::npos;
 }
--- source/QueryEvaluator.h
+++ source/QueryEvaluator.h
@@ -5,6 +5,16 @@ using namespace std;
 
 class QueryEvaluator {
 private:
+	QueryObject *queryObject;
 public:
-	static string evaluateQueryObject(QueryObject *queryObject);
+	QueryEvaluator(QueryObject *queryObject);
+
+	string evaluateQueryObject();
+	string selectImmediateResults();
+	string evaluateUsesClause();
+	string evaluateModifiesClause();
+	string evaluateFollowsClause();
+	string evaluateParentClause();
+
+	bool isInteger(string str);	// to be moved to utility class	
 };
