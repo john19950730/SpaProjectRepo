3b179cfecf7e2caf214d90e9c854c7c5b25dd33b
Joshua <blaqperformance@gmail.com>
2018-09-09 17:51:49 +0800

    Created line of code object which stores data for each line of code

--- source/CodeParser.h
+++ source/CodeParser.h
@@ -5,14 +5,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "LineOfCodeData.h"
 
 using namespace std;
+class LineOfCodeData;
 
 class CodeParser {
 public:
 	static int parse(string);
-	static int CodeParser::processLine(string lineOfCode);
-
+	static int CodeParser::processLine(string lineOfCode, int lineNum);
+	static string CodeParser::tokenize(string keyword, string line);
+	static int CodeParser::checkFollows();
 private:
-
+	static vector<LineOfCodeData> CodeParser::lineData;
+	static stack <string> CodeParser::nesting_level; //save curr nesting level at this line of code
 };
