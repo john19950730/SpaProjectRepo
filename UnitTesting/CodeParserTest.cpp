f55615f7f4e99984529fea2f41cf5d1c05c548d3
ZANARKAND\Daniel <danielkwang@outlook.com>
2018-09-09 18:02:28 +0800

    Created Test Case Files, Commit To fix other matter first

---
+++ UnitTesting/CodeParserTest.cpp
@@ -0,0 +1,19 @@
+#include "CppUnitTest.h"
+#include "CodeParser.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(CodeParserTest)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			TNode T;
+			// TODO: Your test code here
+		}
+
+	};
+}
