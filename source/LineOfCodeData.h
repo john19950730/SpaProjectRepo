f3bbceab9bb2ce2768d4e1bf93f1883b1cf0218a
Joshua <blaqperformance@gmail.com>
2018-09-17 14:17:12 +0800

    Modifies works for assignment, read and container. Uses works for assignment, print. Working on uses for If/While and procedure and Modfies for procedure

--- source/LineOfCodeData.h
+++ source/LineOfCodeData.h
@@ -6,13 +6,14 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <utility>
 
 using namespace std;
 
 class LineOfCodeData {
 public:
 	int store(string type, string actualData
-		, std::stack <std::string> nestlevel);
+		, std::stack <std::pair<int, string>> nestlevel);
 	string getTypeOfData();
 	string getActualData(); // get the whole line of code with no processing
 	std::stack <std::string> getNestingLevel();
@@ -20,6 +21,6 @@ public:
 private:
 	string typeOfData; //whether procedure, read, print, while, if, assign...
 	string actualData; // the actual line of code
-	std::stack <std::string> nesting_level; // pops out the parent of this element and subsequent parents
+	std::stack <std::pair<int, string>> nesting_level; // pops out the parent of this element and subsequent parents
 
 };
