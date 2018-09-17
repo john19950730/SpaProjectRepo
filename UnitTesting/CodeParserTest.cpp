c617c8b139315aef9718b5a117e2bdab5aee115b
ZANARKAND\Daniel <danielkwang@outlook.com>
2018-09-17 18:43:23 +0800

    Added Test Cases For checkModifies

--- UnitTesting/CodeParserTest.cpp
+++ UnitTesting/CodeParserTest.cpp
@@ -11,19 +11,43 @@ namespace UnitTesting
 	{
 	public:
 
-		TEST_METHOD(ParseTest)
-		{
-			int parseVal;
-			parseVal = CodeParser::parse("TestingString");
-			Assert::AreEqual(0, parseVal);
-			// TODO: Your test code here
-		}
+		//TEST_METHOD(ParseTest)
+		//{
+		//	int parseVal;
+		//	parseVal = CodeParser::parse("TestingString");
+		//	Assert::AreEqual(0, parseVal);
+		//}
 
-		TEST_METHOD(ProcessLineTest) 
+		//TEST_METHOD(ProcessLineTest) 
+		//{
+		//	int processLineVal;
+		//	processLineVal = CodeParser::processLine("Test1", 1);
+		//	Assert::AreEqual(0, processLineVal);
+		//}
+
+		TEST_METHOD(CheckModifiesTest) 
 		{
-			int processLineVal;
-			processLineVal = CodeParser::processLine("Test1", 1);
-			Assert::AreEqual(0, processLineVal);
+			string checkModifiesOutput;
+
+			// Testing for assignment of Count
+			checkModifiesOutput = CodeParser::checkModifies("assignment", "count = count + 1");
+			string intendedOutput = "count";
+			Assert::AreEqual(intendedOutput, checkModifiesOutput);
+
+			//Testing for wrong input of Count
+			checkModifiesOutput = CodeParser::checkModifies("assignment", "acount = count + 1");
+			intendedOutput = "count";
+			Assert::AreNotEqual(intendedOutput, checkModifiesOutput);
+
+			// Testing for Read
+			checkModifiesOutput = CodeParser::checkModifies("read", "a");
+			intendedOutput = "a";
+			Assert::AreEqual(intendedOutput, checkModifiesOutput);
+
+			// Testing for 
+			checkModifiesOutput = CodeParser::checkModifies("read", "aa");
+			intendedOutput = "a";
+			Assert::AreNotEqual(intendedOutput, checkModifiesOutput);
 		}
 
 	};
